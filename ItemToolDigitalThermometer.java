448e3e65a25ce39b025e63c0414840e5e28cf87e
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-04-29 20:19:46 -0700

    Add Nuclear Control v1.1.2 decompiled with Bukkit 1.2.5 mappings

---
+++ ItemToolDigitalThermometer.java
@@ -0,0 +1,63 @@
+package nuclearcontrol;
+
+import ic2.api.ElectricItem;
+import ic2.api.IElectricItem;
+import net.minecraft.src.EntityHuman;
+import net.minecraft.src.ItemStack;
+
+public class ItemToolDigitalThermometer extends ItemToolThermometer implements IElectricItem
+{
+    public int tier;
+    public int ratio;
+    public int transfer;
+
+    public ItemToolDigitalThermometer(int var1, int var2, ThermometerVersion var3, int var4, int var5, int var6)
+    {
+        super(var1, var2, var3);
+        this.setMaxDurability(101);
+        this.tier = var4;
+        this.ratio = var5;
+        this.transfer = var6;
+    }
+
+    public boolean canTakeDamage(ItemStack var1, int var2)
+    {
+        var2 *= 50;
+        return ElectricItem.discharge(var1, var2, Integer.MAX_VALUE, true, true) == var2;
+    }
+
+    public void damage(ItemStack var1, int var2, EntityHuman var3)
+    {
+        ElectricItem.use(var1, 50 * var2, var3);
+    }
+
+    public boolean canProvideEnergy()
+    {
+        return false;
+    }
+
+    public int getChargedItemId()
+    {
+        return this.id;
+    }
+
+    public int getEmptyItemId()
+    {
+        return this.id;
+    }
+
+    public int getMaxCharge()
+    {
+        return 12000;
+    }
+
+    public int getTier()
+    {
+        return this.tier;
+    }
+
+    public int getTransferLimit()
+    {
+        return 250;
+    }
+}
---
+++ ItemToolThermometer.java
@@ -0,0 +1,126 @@
+package nuclearcontrol;
+
+import forge.ITextureProvider;
+import ic2.api.ElectricItem;
+import net.minecraft.src.EntityHuman;
+import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.TileEntity;
+import net.minecraft.src.World;
+import net.minecraft.src.mod_IC2NuclearControl;
+
+public class ItemToolThermometer extends Item implements ITextureProvider
+{
+    public ThermometerVersion thermometerVersion;
+
+    public ItemToolThermometer(int var1, int var2, ThermometerVersion var3)
+    {
+        super(var1);
+        this.d(var2);
+        this.setMaxDurability(102);
+        this.e(1);
+        this.thermometerVersion = var3;
+    }
+
+    public boolean canTakeDamage(ItemStack var1, int var2)
+    {
+        return true;
+    }
+
+    public String getTextureFile()
+    {
+        return "/img/texture_thermo.png";
+    }
+
+    public boolean onItemUseFirst(ItemStack var1, EntityHuman var2, World var3, int var4, int var5, int var6, int var7)
+    {
+        if (!this.canTakeDamage(var1, 2))
+        {
+            return false;
+        }
+        else
+        {
+            TileEntity var8 = NuclearHelper.getReactorAt(var3, var4, var5, var6);
+
+            if (var8 == null)
+            {
+                TileEntity var9 = NuclearHelper.getReactorChamberAt(var3, var4, var5, var6);
+
+                if (var9 != null)
+                {
+                    var8 = NuclearHelper.getReactorAroundCoord(var3, var4, var5, var6);
+                }
+            }
+
+            if (var8 != null)
+            {
+                if (!var3.isStatic)
+                {
+                    this.messagePlayer(var2, var8);
+                    this.damage(var1, 1, var2);
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public void messagePlayer(EntityHuman var1, TileEntity var2)
+    {
+        int var3 = NuclearHelper.getReactorHeat(var2);
+
+        switch (ItemToolThermometer.NamelessClass1845284373.$SwitchMap$net$minecraft$src$nuclearcontrol$ThermometerVersion[this.thermometerVersion.ordinal()])
+        {
+            case 1:
+                mod_IC2NuclearControl.chatMessage(var1, "Hull heat: " + var3);
+                break;
+
+            case 2:
+                int var4 = NuclearHelper.getMaxHeat(var2);
+                mod_IC2NuclearControl.chatMessage(var1, "Hull heat: " + var3 + " (Water evaporate: " + var4 * 50 / 100 + " / melting: " + var4 * 85 / 100 + ")");
+        }
+    }
+
+    public void damage(ItemStack var1, int var2, EntityHuman var3)
+    {
+        switch (ItemToolThermometer.NamelessClass1845284373.$SwitchMap$net$minecraft$src$nuclearcontrol$ThermometerVersion[this.thermometerVersion.ordinal()])
+        {
+            case 1:
+                var1.damage(10, var3);
+                break;
+
+            case 2:
+                ElectricItem.use(var1, 50 * var2, var3);
+        }
+    }
+
+    static class NamelessClass1845284373
+    {
+        static final int[] $SwitchMap$net$minecraft$src$nuclearcontrol$ThermometerVersion = new int[ThermometerVersion.values().length];
+
+        static
+        {
+            try
+            {
+                $SwitchMap$net$minecraft$src$nuclearcontrol$ThermometerVersion[ThermometerVersion.ANALOG.ordinal()] = 1;
+            }
+            catch (NoSuchFieldError var2)
+            {
+                ;
+            }
+
+            try
+            {
+                $SwitchMap$net$minecraft$src$nuclearcontrol$ThermometerVersion[ThermometerVersion.DIGITAL.ordinal()] = 2;
+            }
+            catch (NoSuchFieldError var1)
+            {
+                ;
+            }
+        }
+    }
+}
