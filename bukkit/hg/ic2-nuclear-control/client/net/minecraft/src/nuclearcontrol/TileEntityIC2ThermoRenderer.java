63b031bd4be70e707b42e64a9b8ad5286500ab5d
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-05-28 12:14:35 -0700

    Update hg

--- bukkit/hg/ic2-nuclear-control/client/net/minecraft/src/nuclearcontrol/TileEntityIC2ThermoRenderer.java
+++ bukkit/hg/ic2-nuclear-control/client/net/minecraft/src/nuclearcontrol/TileEntityIC2ThermoRenderer.java
@@ -1,5 +1,6 @@
 package net.minecraft.src.nuclearcontrol;
 
+import net.minecraft.src.Facing;
 import net.minecraft.src.FontRenderer;
 import net.minecraft.src.TileEntity;
 import net.minecraft.src.TileEntitySpecialRenderer;
@@ -17,7 +18,7 @@ public class TileEntityIC2ThermoRenderer extends TileEntitySpecialRenderer
         {
             GL11.glPushMatrix();
             TileEntityIC2Thermo thermo = (TileEntityIC2Thermo)tileEntity;
-            short side = thermo.getFacing();
+            short side = (short)Facing.faceToSide[thermo.getFacing()];
             float var12 = 0.016666668F;
             int heat = (thermo.getHeatLevel() / 500) * 500;
             String text = Integer.toString(heat);
@@ -53,14 +54,15 @@ public class TileEntityIC2ThermoRenderer extends TileEntitySpecialRenderer
                     break;
 
             }
-            GL11.glTranslatef(0.5F, 0.4377F, 0.6875F);
+            GL11.glTranslatef(0.5F, 0.4375F, 0.6875F);
             
             FontRenderer fontRenderer = this.getFontRenderer();
             GL11.glRotatef(-90, 1, 0, 0);
             GL11.glScalef(var12, - var12, var12);
-            GL11.glDepthMask(false);
+            GL11.glPolygonOffset( -1, -1 );
+            GL11.glEnable ( GL11.GL_POLYGON_OFFSET_FILL );
             fontRenderer.drawString(text, -fontRenderer.getStringWidth(text) / 2, -fontRenderer.FONT_HEIGHT, 0);
-            GL11.glDepthMask(true);
+            GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL );
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             GL11.glPopMatrix();
         }
